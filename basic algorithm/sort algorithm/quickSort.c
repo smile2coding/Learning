5adafbac396c4c780a3afc1077d9a327d2169865
HaoDu <hao.du@siemens.com>
2022-10-28 12:52:36 +0800

    feat: add quick sort.

--- basic algorithm/sort algorithm/quickSort.c
+++ basic algorithm/sort algorithm/quickSort.c
@@ -1,11 +1,41 @@
 #include "stdio.h"
 
-int quickSort(int arr[], int len)
+int quickSort(int arr[], int left, int right)
 {
+    int low = left, high = right;
+    int pivot = arr[low];
+    if (low < high)
+    {
+        while (low < high)
+        {
+            while (low < high && pivot <= arr[high])
+            {
+                high--;
+            }
+            if (low < high)
+                arr[low++] = arr[high];
+            while (low < high && pivot > arr[low])
+            {
+                low++;
+            }
+            if (low < high)
+                arr[high--] = arr[low];
+        }
+        arr[low] = pivot;
+        quickSort(arr, left, low - 1);
+        quickSort(arr, low + 1, right);
+    }
     return 0;
 }
 
 int main()
 {
-    return 0;
+    int testArray[10] = {1, 4, 7, 9, 20, 12, 3, 4, 6, 7};
+    quickSort(testArray, 0, 9);
+    for (int i = 0; i < 10; i++)
+    {
+        printf("%d\n\r", testArray[i]);
+    }
+    while (1)
+        ;
 }
